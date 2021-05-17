2ba8447f7e6321be125cd3ccddab0a22341a5c19
Rasmus Siljander <rasmus.siljander@almamedia.fi>
2021-05-17 16:33:41 +0300

    testing show_conversion_slices() functionality

--- png2nii/test_conversion.py
+++ png2nii/test_conversion.py
@@ -1,18 +1,22 @@
-from png2nii import convert_to_nifti
+from png2nii import convert_to_nifti, show_sample_slices
 import os
 import nibabel as nib
 
 
 def main():
     current_dir = os.getcwd()
-    png_dir = os.path.join(current_dir, 'png-imgs')         #Specify png images folder here
+    png_dir = os.path.join(current_dir, 'png2nii', 'png-imgs')         #Specify png images folder here
     dst_filename = 'png2nii_test.nii'
 
     "Reading a sample header"
-    sample_nii = os.path.join(current_dir, 'rt1.nii')
+    sample_nii = os.path.join(current_dir, 'png2nii' ,'rt1.nii')
     img_header = nib.load(sample_nii).header
 
     convert_to_nifti(png_dir, dst_filename, img_header)
-
+    
+    " (show sample slices) "
+    #sample_nii2 = os.path.join(current_dir, 'png2nii' ,'png2nii_test.nii')
+    #show_sample_slices(sample_nii2)
+    
 if __name__ == '__main__':
     main()
